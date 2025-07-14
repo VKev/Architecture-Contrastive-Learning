a7f78810a9406ceb78d4a29d204524a7a89fa041
huynh khang <huynhkhang7452@gmail.com>
2025-07-14 10:18:27 +0700

    Update

--- util/transform.py
+++ util/transform.py
@@ -17,12 +17,12 @@ transform_cifar10_train = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.RandomCrop(32, padding=4),
     transforms.ToTensor(),  
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 
 transform_cifar10_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 
 transform_imagenet_train = transforms.Compose([
